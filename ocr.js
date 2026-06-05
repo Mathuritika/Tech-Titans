6e1a027dcdc67bca28599c3ff909322fa6f44f42
Shivangi Thapliyal <shivangithapliyal25@gmail.com>
2026-06-06 00:28:01 +0530

    Added OCR module files

--- ocr.js
+++ ocr.js
@@ -2,11 +2,7 @@ const Tesseract = require('tesseract.js');
 
 async function extractText(imagePath) {
     try {
-        const result = await Tesseract.recognize(
-            imagePath,
-            'eng'
-        );
-
+        const result = await Tesseract.recognize(imagePath, 'eng');
         return result.data.text;
     } catch (error) {
         console.error(error);
